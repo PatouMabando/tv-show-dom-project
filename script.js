a18795cfd193eebee26a39e117bf46e6e26d541c
Patou Malongi Mabando <patoumabando@gmail.com>
2023-02-18 16:40:48 +0200

    tv-show-dom-projet-level-100

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-//You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const container = document.getElementById("episodes");
+
+  episodeList.forEach((episode) => {
+      const card = document.createElement("div");
+      card.classList.add("card");
+
+      const h1 = document.createElement("h1");
+      h1.textContent = `${episode.name} - ${makeEpisodeCode(episode)}`;
+
+      const img = document.createElement("img");
+      img.setAttribute("src", episode.image.medium);
+
+      const p = document.createElement("p");
+      p.textContent = episode.summary;
+
+      container.appendChild(card);
+      card.appendChild(h1);
+      card.appendChild(img);
+      card.appendChild(p);
+  });
+}
+
+function makeEpisodeCode(episode) {
+  return `S${pad(episode.season)}E${pad(episode.number)}`;
+}
+
+function pad(num) {
+  return num.toString().padStart(2, "0");
 }
 
 window.onload = setup;
